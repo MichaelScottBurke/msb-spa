7cd1e80e0f3113304ec8293ffc76025f7a2f44c8
Michael Scott Burke <micky68@gmail.com>
2023-10-09 13:03:11 -0400

    Update page3.js

--- page3.js
+++ page3.js
@@ -1,19 +1,6 @@
-function testClickEventOld() {
-  alert("click event with button onclick working in Forge");
-  console.log("click event with button onclick working in Forge");
-}
-document.getElementById("btn-test-3").addEventListener("click", function () {
-  console.log("test 3");
-  alert("Test 3");
-});
-document.addEventListener("DOMContentLoaded", () => {
-  testClickEvent();
-  console.log("DOM fully loaded and parsed");
-  document.getElementById("btn-test-3").addEventListener("click", function () {
-    console.log("event listener test 3 added after dom loaded");
-    alert("Test 3");
-  });
-});
 window.addEventListener("load", (event) => {
   console.log("page is fully loaded");
+});
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("dom is fully loaded");
 });
