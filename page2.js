d7d6f04203141b6f82c0a6bcda9d41e2a18b11df
Michael Scott Burke <micky68@gmail.com>
2023-10-09 09:56:36 -0400

    Create page2.js

---
+++ page2.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM loaded");
+  const btnTestListeners = document.querySelector("#btn-test-listeners");
+  const btnTestListeners2 = document.querySelector("#btn-test-listeners2");
+  btnTestListeners.addEventListener("click", testListeners);
+  btnTestListeners2.addEventListener("click", testListeners2);
+
+  function testListeners() {
+    console.log("Testing listener");
+    alert("Listener tested");
+  }
+
+  function testListeners2() {
+    console.log("Testing listener 2");
+    alert("Listener 2 tested");
+  }
+});
