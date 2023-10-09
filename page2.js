c5f177454ae448741f99616e4542c386d5cbfe31
Michael Scott Burke <micky68@gmail.com>
2023-10-09 11:47:36 -0400

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,14 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM loaded");
-  testListeners();
-  testListeners2();
-  const btnTestListeners = document.querySelector("#btn-test-listeners");
-  const btnTestListeners2 = document.querySelector("#btn-test-listeners2");
-  btnTestListeners.addEventListener("click", testListeners);
-  btnTestListeners2.addEventListener("click", testListeners2);
+console.log("DOM loaded");
+testListeners();
+testListeners2();
+const btnTestListeners = document.querySelector("#btn-test-listeners");
+const btnTestListeners2 = document.querySelector("#btn-test-listeners2");
+btnTestListeners.addEventListener("click", testListeners);
+btnTestListeners2.addEventListener("click", testListeners2);
 
-
-});
 function testListeners() {
   console.log("Testing listener");
   alert("Listener tested");
